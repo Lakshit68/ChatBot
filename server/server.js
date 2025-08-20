a02f59236fa2fb7904580ec5bbf95950c5872218
lakshit68 <lakshitsachdeva25@gmail.com>
2025-08-20 23:03:43 +0530

    update

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const io = new Server(server, {
 });
 
 // Connect MongoDB
-const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
+const mongoUri = process.env.MONGO_URI ;
 await mongoose.connect(mongoUri, { dbName: 'chatbot_app' });
 
 // Middleware
